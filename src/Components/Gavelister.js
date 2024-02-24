53b003a168d38a7f58b92c34b7f413d989d494ac
Erik Hole <erik.hole@norskdatateknikk.no>
2024-02-24 11:14:20 +0100

    close #7
    Signed-off-by: Erik Hole <erik.hole@norskdatateknikk.no>

--- src/Components/Gavelister.js
+++ src/Components/Gavelister.js
@@ -15,8 +15,8 @@ const Gavelister = () => {
         Pengegaver kan sendes til Vipps-nummer
         <span className="font-weight-bold"> 90811303</span> eller til
         kontonummer
-        <span className="font-weight-bold"> 1226.94.38558</span>. Merk gjerne
-        med "bryllupsgave".
+        <span className="font-weight-bold"> 1226.94.38558</span>. Merk med
+        "bryllupsgave" + navn.
       </p>
     </Container>
   );
